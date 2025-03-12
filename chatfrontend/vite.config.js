c61abf09111c865655415f27bfa0c3ba82b97de8
aniket709 <142618124+aniket709@users.noreply.github.com>
2025-03-12 19:47:38 +0530

    Update vite.config.js

--- chatfrontend/vite.config.js
+++ chatfrontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ base: "chatApplication",
   plugins: [
     tailwindcss(),
     react()],
